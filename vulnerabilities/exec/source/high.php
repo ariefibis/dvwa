815389619b2e9b9a7e79c709473dcfc0a50e0562
Xiao Li <xli@officeworks.com.au>
2024-10-22 16:25:38 +1100

    fix: bug

--- vulnerabilities/exec/source/high.php
+++ vulnerabilities/exec/source/high.php
@@ -1,37 +1,23 @@
 <?php
 
-if( isset( $_POST[ 'Submit' ]  ) ) {
-	// Get input
-	$target = trim($_REQUEST[ 'ip' ]);
+if (isset($_POST['Submit'])) {
+    // Get input
+    $target = trim($_REQUEST['ip']);
 
-	// Set blacklist
-	$substitutions = array(
-		'&'  => '',
-		';'  => '',
-		'| ' => '',
-		'-'  => '',
-		'$'  => '',
-		'('  => '',
-		')'  => '',
-		'`'  => '',
-		'||' => '',
-	);
+    // Use escapeshellarg to safely escape the input
+    $target = escapeshellarg($target);
 
-	// Remove any of the characters in the array (blacklist).
-	$target = str_replace( array_keys( $substitutions ), $substitutions, $target );
+    // Determine OS and execute the ping command.
+    if (stristr(php_uname('s'), 'Windows NT')) {
+        // Windows
+        $cmd = shell_exec('ping ' . $target);
+    } else {
+        // *nix
+        $cmd = shell_exec('ping -c 4 ' . $target);
+    }
 
-	// Determine OS and execute the ping command.
-	if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
-		// Windows
-		$cmd = shell_exec( 'ping  ' . $target );
-	}
-	else {
-		// *nix
-		$cmd = shell_exec( 'ping  -c 4 ' . $target );
-	}
-
-	// Feedback for the end user
-	$html .= "<pre>{$cmd}</pre>";
+    // Feedback for the end user
+    $html .= "<pre>" . htmlspecialchars($cmd, ENT_QUOTES, 'UTF-8') . "</pre>";
 }
 
 ?>
