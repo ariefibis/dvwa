bb85b33518cecd938080242f9f8f8cd171a37736
Vikas Sharma <vsharma@officeworks.com.au>
2024-10-22 15:49:59 +1100

    fix cwe-89 - 2

--- vulnerabilities/sqli/source/low.php
+++ vulnerabilities/sqli/source/low.php
@@ -8,7 +8,7 @@ if( isset( $_REQUEST[ 'Submit' ] ) ) {
 		case MYSQL:
 			// Check database
 			$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-			$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+			$result = mysql_query("SELECT * FROM projects WHERE id='" . mysql_real_escape_string($_GET['id']) . "'") or die("Could not query");
 
 			// Get results
 			while( $row = mysqli_fetch_assoc( $result ) ) {
