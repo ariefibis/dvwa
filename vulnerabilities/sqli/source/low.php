6afb49f046d30487cd0571b32f778c64248c81be
Xiao Li <xli@officeworks.com.au>
2024-10-22 16:21:59 +1100

    fix: vul

--- vulnerabilities/sqli/source/low.php
+++ vulnerabilities/sqli/source/low.php
@@ -1,56 +1,77 @@
 <?php
 
-if( isset( $_REQUEST[ 'Submit' ] ) ) {
-	// Get input
-	$id = $_REQUEST[ 'id' ];
+if (isset($_REQUEST['Submit'])) {
+    // Get input
+    $id = $_REQUEST['id'];
 
-	switch ($_DVWA['SQLI_DB']) {
-		case MYSQL:
-			// Check database
-			$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-			$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+    switch ($_DVWA['SQLI_DB']) {
+        case MYSQL:
+            // Prepare the SQL query using prepared statements
+            $stmt = $GLOBALS["___mysqli_ston"]->prepare("SELECT first_name, last_name FROM users WHERE user_id = ?");
+            
+            if (!$stmt) {
+                die('<pre>Error preparing statement: ' . $GLOBALS["___mysqli_ston"]->error . '</pre>');
+            }
 
-			// Get results
-			while( $row = mysqli_fetch_assoc( $result ) ) {
-				// Get values
-				$first = $row["first_name"];
-				$last  = $row["last_name"];
+            // Bind the parameter
+            $stmt->bind_param('i', $id);  // 'i' indicates the parameter type is an integer
 
-				// Feedback for end user
-				$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
-			}
+            // Execute the query
+            $stmt->execute();
 
-			mysqli_close($GLOBALS["___mysqli_ston"]);
-			break;
-		case SQLITE:
-			global $sqlite_db_connection;
+            // Get the result
+            $result = $stmt->get_result();
 
-			#$sqlite_db_connection = new SQLite3($_DVWA['SQLITE_DB']);
-			#$sqlite_db_connection->enableExceptions(true);
+            // Fetch the result
+            while ($row = $result->fetch_assoc()) {
+                // Get values
+                $first = htmlspecialchars($row['first_name'], ENT_QUOTES, 'UTF-8');
+                $last  = htmlspecialchars($row['last_name'], ENT_QUOTES, 'UTF-8');
 
-			$query  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-			#print $query;
-			try {
-				$results = $sqlite_db_connection->query($query);
-			} catch (Exception $e) {
-				echo 'Caught exception: ' . $e->getMessage();
-				exit();
-			}
+                // Feedback for end user
+                $html .= "<pre>ID: " . htmlspecialchars($id, ENT_QUOTES, 'UTF-8') . "<br />First name: {$first}<br />Surname: {$last}</pre>";
+            }
 
-			if ($results) {
-				while ($row = $results->fetchArray()) {
-					// Get values
-					$first = $row["first_name"];
-					$last  = $row["last_name"];
+            // Close the statement and connection
+            $stmt->close();
+            mysqli_close($GLOBALS["___mysqli_ston"]);
+            break;
 
-					// Feedback for end user
-					$html .= "<pre>ID: {$id}<br />First name: {$first}<br />Surname: {$last}</pre>";
-				}
-			} else {
-				echo "Error in fetch ".$sqlite_db->lastErrorMsg();
-			}
-			break;
-	} 
+        case SQLITE:
+            global $sqlite_db_connection;
+
+            // Prepare the SQL query using SQLite3 prepared statements
+            $query = "SELECT first_name, last_name FROM users WHERE user_id = :id";
+            $stmt = $sqlite_db_connection->prepare($query);
+
+            if (!$stmt) {
+                echo 'Error preparing SQLite statement: ' . $sqlite_db_connection->lastErrorMsg();
+                exit();
+            }
+
+            // Bind the parameter
+            $stmt->bindValue(':id', $id, SQLITE3_INTEGER);
+
+            // Execute the query
+            $results = $stmt->execute();
+
+            // Fetch the result
+            if ($results) {
+                while ($row = $results->fetchArray(SQLITE3_ASSOC)) {
+                    // Get values
+                    $first = htmlspecialchars($row['first_name'], ENT_QUOTES, 'UTF-8');
+                    $last  = htmlspecialchars($row['last_name'], ENT_QUOTES, 'UTF-8');
+
+                    // Feedback for end user
+                    $html .= "<pre>ID: " . htmlspecialchars($id, ENT_QUOTES, 'UTF-8') . "<br />First name: {$first}<br />Surname: {$last}</pre>";
+                }
+            } else {
+                echo "Error in fetch: " . $sqlite_db_connection->lastErrorMsg();
+            }
+
+            // Close the statement and connection
+            $stmt->close();
+            break;
+    }
 }
-
 ?>
